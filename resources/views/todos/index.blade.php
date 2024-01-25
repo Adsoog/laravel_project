2a3af684aae662e63ff1763714d1435f40526159
unknown <iadso.ao@gmail.com>
2024-01-24 22:28:16 -0500

    parafel

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,14 @@
+@extends('projecto')
+
+@section('content')
+    <div class="container w-25 border p-4 mt-4" >
+        <form action="{{ route('todos') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Titulo de la tarea</label>
+                <input type="text" name="title" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+@endsection
